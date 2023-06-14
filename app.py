49516e65ea3a53ed19af723c93341cebea30b91c
ahmad-alkadri <ahmad.alkadri@outlook.com>
2023-06-15 00:07:45 +0200

    First working fix: tweet scraper works

--- app.py
+++ app.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 from matplotlib.font_manager import FontProperties
 import streamlit as st
 import src.helperfuns as hlp
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 
 # SECTION - INITIATION -----
 # Configuring the initiation of the streamlit app
 
 # Page title
 st.set_page_config(page_title="Tweet Cloud Maker")
+
+# Tweet scraper client
+twtdig = hlp.TweetDigester()
 
 # Hide the hamburger menu
 st.markdown("""
@@ -36,173 +37,131 @@ st.markdown(
 	username, any username, as long as their profile is public 
 	and they have tweets in their profile. 
 	
-	Go to the sidebar on the left and select the 
-	scraping mode that you want, input the username and the number of 
-	tweets that you want to search *or* the date interval, and 
+	Go to the sidebar, input the username and the number of 
+	tweets that you want to search, and 
 	click Submit. Below, you'll find the word cloud. **Warnings**:
 	
 	+ the whole process could take between 30—60 seconds
-	+ at maximum only 2000 tweets will be scraped
+	+ at maximum only 1000 tweets will be scraped
 	"""
 )
 
 # SECTION - CONFIG PART -----
-# This section is dedicated to parts used for configuring
-# the twitter scrape, starting from selecting the mode
-# of search (either through scraping based on the number
-# of last tweets or selecting tweets within a certain
-# interval date)
-
-modescrape = st.sidebar.selectbox(
-    "Select scraping mode:",
-    ["Date interval", "Number of last tweets"],
-    index=1)
-
-# Prepare the interval date
-nowdate = datetime.now()
-ayearago = nowdate - relativedelta(years=1)
-
 # Sidebar forms for the config menu
-if modescrape == "Number of last tweets":
-    with st.sidebar.form("formquantity"):
-        usernamereq = st.text_input(
-            label="Input the twitter username",
-            value="@jack")
-        numtweets = st.number_input(
-            label="Number of tweets to be scraped",
-            value=100,
-            step=1, min_value=1, max_value=2000)
-        subt = st.form_submit_button()
-elif modescrape == "Date interval":
-    with st.sidebar.form("formdate"):
-        usernamereq = st.text_input(
-            label="Input the twitter username",
-            value="@jack")
-        datestart = st.date_input("Start date:",
-                                  value=ayearago,
-                                  min_value=ayearago,
-                                  max_value=nowdate)
-        dateend = st.date_input("End date:",
-                                value=nowdate,
-                                min_value=ayearago,
-                                max_value=nowdate)
-        dateend_str = dateend.strftime("%Y-%m-%d")
-        datestart_str = datestart.strftime("%Y-%m-%d")
-        subt = st.form_submit_button()
+with st.sidebar.form("formquantity"):
+    usernamereq = st.text_input(
+        label="Input the twitter username",
+        value="@jack")
+    numtweets = st.number_input(
+        label="Number of tweets to be scraped",
+        value=100,
+        step=1, min_value=1, max_value=1000)
+    skipreplies = st.checkbox("Skip replies")
+    subt = st.form_submit_button()
 
 # Button to clear caches
-st.sidebar.info("""Click the button below to clear all cached tweets 
-    that were scraped""")
-clcbut = st.sidebar.button("Clear Cache")
+# st.sidebar.info("""Click the button below to clear all cached tweets 
+#     that were scraped""")
+# clcbut = st.sidebar.button("Clear Cache")
 
-if clcbut:
-    st.experimental_memo.clear()
-    st.experimental_singleton.clear()
+# if clcbut:
+#     st.cache_data.clear()
+#     st.cache_resource.clear()
 
 # SECTION - SCRAPING PART -----
 # This section is dedicated to the tweet-scraping operations.
 # It takes the input by users made on the CONFIG section
 
 # Scrape the tweets
-if modescrape == "Number of last tweets" and subt:
-    dft = hlp.gettweetamount(
-        username=usernamereq.replace("@", ""),
-        amount=numtweets
-    )
-elif modescrape == "Date interval" and subt:
-    dft = hlp.gettweetdates(
-        username=usernamereq.replace("@", ""),
-        startdate=datestart_str,
-        enddate=dateend_str
-    )
+if subt:
+    with st.spinner("Scraping..."):
+        try:
+            twtdig.get_tweets_user(usernamereq, numtweets, skipreplies)
+        except Exception:
+            st.error("Error during tweet scraping")
 
-# SECTION - WORDCLOUD PART -----
-# This section takes the output from the tweet-scraping
-# operations and make the wordcloud from it, including
-# displaying it on the page.
+    # SECTION - WORDCLOUD PART -----
+    # This section takes the output from the tweet-scraping
+    # operations and make the wordcloud from it, including
+    # displaying it on the page.
 
-if 'dft' in globals():
-    tweets = dft['Text'].to_list()
+    with st.spinner("Preparing the wordcloud..."):
+        alltweetsclean = twtdig.get_cleaned_tweets()
 
-    alltweets = " ".join(tweets)
-    alltweets = alltweets.replace("\n", "")
-    # Split to list of texts
-    alltweets = alltweets.split(" ")
-    # Clean up
-    alltweetsclean = hlp.cleanlststr(alltweets)
-    wordsclean = ' '.join(alltweetsclean)
-    # Capitalize everything
-    wordsready = wordsclean.upper()
+        # Clean up
+        wordsclean = ' '.join(alltweetsclean)
+        # Capitalize everything
+        wordsready = wordsclean.upper()
 
-    # Prepare the mask
-    mask = np.array(
-        Image.open(
-            os.path.join(
-                os.getcwd(), "assets", "twitterlogo.png"
+        # Prepare the mask
+        mask = np.array(
+            Image.open(
+                os.path.join(
+                    os.getcwd(), "assets", "twitterlogo.png"
+                )
             )
         )
-    )
-    # Replace the transparent value with white
-    maskcorr = np.where(mask == 0, 255, mask)
+        # Replace the transparent value with white
+        maskcorr = np.where(mask == 0, 255, mask)
 
-    # Get the font file
-    path2font = os.path.join(
-        os.getcwd(), "assets", "cloudfont",
-        "NotoSansKR-Medium.otf"
-    )
-
-    # Make the word cloud
-    try:
-        word_cloud = hlp.getwordcloud(
-            path2font,
-            width=1600,
-            height=1600,
-            words=wordsready,
-            mask=maskcorr
+        # Get the font file
+        path2font = os.path.join(
+            os.getcwd(), "assets", "cloudfont",
+            "NotoSansKR-Medium.otf"
         )
-        statcloud = True
-    except ValueError as err:
-        print(err)
-        st.warning("""
-			**ERROR: username not found or tweets unsearchable.**
-			Please make sure that:\n
-			1. the twitter profile exists\n
-			2. the twitter profile is public\n
-			3. the user has tweeted at least once
-		""")
-        statcloud = False
 
-    if statcloud:
-        st.header("TweetCloud")
-        st.markdown("""
-			Right-click the image below and select "Save Image As" 
-			to get the image.
-		""")
-        # Make the image
-        font_prop = FontProperties(fname=path2font)
-        fig, ax = plt.subplots()
-        ax.imshow(word_cloud)
-        # Text username
-        ax.text(
-            0.5, 0.925,
-            f'{usernamereq}',
-            horizontalalignment='center',
-            fontsize=7.5,
-            font_properties=font_prop,
-            color='white',
-            transform=ax.transAxes)
-        # Text credit
-        ax.text(
-            0.5, 0.05,
-            'tweetcloudmaker.streamlit.app',
-            horizontalalignment='center',
-            fontsize=5,
-            font_properties=font_prop,
-            color='white',
-            transform=ax.transAxes)
-        ax.axis('off')
-        ax.set_facecolor('#1DA1F2')
-        st.pyplot(fig, pad_inches=0)
+        # Make the word cloud
+        try:
+            word_cloud = hlp.getwordcloud(
+                path2font,
+                width=1600,
+                height=1600,
+                words=wordsready,
+                mask=maskcorr
+            )
+            statcloud = True
+        except ValueError as err:
+            print(err)
+            st.warning("""
+                **ERROR: username not found or tweets unsearchable.**
+                Please make sure that:\n
+                1. the twitter profile exists\n
+                2. the twitter profile is public\n
+                3. the user has tweeted at least once
+            """)
+            statcloud = False
+
+        if statcloud:
+            st.header("TweetCloud")
+            st.markdown("""
+                Right-click the image below and select "Save Image As" 
+                to get the image.
+            """)
+            # Make the image
+            font_prop = FontProperties(fname=path2font)
+            fig, ax = plt.subplots()
+            ax.imshow(word_cloud)
+            # Text username
+            ax.text(
+                0.5, 0.925,
+                f'{usernamereq}',
+                horizontalalignment='center',
+                fontsize=7.5,
+                font_properties=font_prop,
+                color='white',
+                transform=ax.transAxes)
+            # Text credit
+            ax.text(
+                0.5, 0.05,
+                'tweetcloudmaker.streamlit.app',
+                horizontalalignment='center',
+                fontsize=5,
+                font_properties=font_prop,
+                color='white',
+                transform=ax.transAxes)
+            ax.axis('off')
+            ax.set_facecolor('#1DA1F2')
+            st.pyplot(fig, pad_inches=0)
 
 st.markdown("---")
 st.header("Contact")
--- src/helperfuns.py
+++ src/helperfuns.py
@@ -1,108 +1,45 @@
 # Importing libraries and packages
-import snscrape.modules.twitter as sntwitter
+from pytterrator import Client
 from wordcloud import WordCloud, STOPWORDS
-import pandas as pd
-import emoji
 import streamlit as st
+import re
+import unicodedata
 
 
-def emojidetect(text):
-    stat = any(
-        [c in emoji.UNICODE_EMOJI['en'] for c in text]
-    )
-    return stat
+class TweetDigester:
+    def __init__(self):
+        self.client = Client()
+        self.arrtweets = [""]
+
+    def get_tweets_user(self, username, numtweet, exclude_replies: bool = False):
+        self.arrtweets = self.client.getprecisenumtweetstext(
+            username,
+            count=numtweet,
+            exclude_replies=False,
+            include_rts=False,
+            limit_singlereq=20,
+        )
+
+    def get_cleaned_tweets(self):
+        words = []
+
+        # Pattern to match words with alphanumeric characters, Chinese, Japanese, and Korean characters
+        pattern = re.compile(
+            r"^[\w\u4e00-\u9fff\u3040-\u309f\u30a0-\u30ff\uac00-\ud7af]+$"
+        )
+
+        # Split each text into words and process each word
+        for text in self.arrtweets:
+            for word in text.split(" "):
+                if "http://" in word:  # remove word containing http://
+                    continue
+                if pattern.match(word):  # add word only if it matches the pattern
+                    words.append(word)
+        return words
 
 
-@st.experimental_memo
-def gettweetdates(username, startdate, enddate):
-    # Creating list to append tweet data
-    tweets_list = []
-    i = 0
-    uname = f'from:{username}'
-    sincedate = f'since:{startdate}'
-    untildate = f'until:{enddate}'
-    scrapestr = ' '.join([
-        uname, sincedate, untildate
-    ])
-    # Using TwitterSearchScraper to scrape data
-    # and append tweets to list
-    for j, twt in enumerate(
-            sntwitter.TwitterSearchScraper(
-            scrapestr
-            ).get_items()):
-        i += 1
-        if i > 2000:
-            break
-        tweets_list.append([
-            twt.date,
-            twt.id,
-            twt.content,
-            twt.user.username
-        ])
-
-    # Creating a dataframe from the tweets list above
-    tweets_df = pd.DataFrame(
-        tweets_list,
-        columns=[
-            'Datetime',
-            'Tweet Id',
-            'Text',
-            'Username'
-        ]
-    )
-    # Return the data
-    return tweets_df
-
-
-@st.experimental_memo
-def gettweetamount(username, amount=500):
-    # Creating list to append tweet data
-    tweets_list = []
-    i = 0
-    scrapestr = f'from:{username}'
-    # Using TwitterSearchScraper to scrape data
-    # and append tweets to list
-    for j, twt in enumerate(
-            sntwitter.TwitterSearchScraper(
-            scrapestr
-            ).get_items()):
-        i += 1
-        if i > amount:
-            break
-        tweets_list.append([
-            twt.date,
-            twt.id,
-            twt.content,
-            twt.user.username
-        ])
-
-    # Creating a dataframe from the tweets list above
-    tweets_df = pd.DataFrame(
-        tweets_list,
-        columns=[
-            'Datetime',
-            'Tweet Id',
-            'Text',
-            'Username'
-        ]
-    )
-    # Return the data
-    return tweets_df
-
-
-@st.experimental_memo
-def cleanlststr(texts):
-    cleantexts = []
-    for words in texts:
-        if emojidetect(words) or not(words.isalnum()):
-            continue
-        else:
-            cleantexts.append(words)
-    return cleantexts
-
-@st.experimental_singleton
-def getwordcloud(path2font, width, height,
-                 words, mask, backgroundcolor='#1DA1F2'):
+@st.cache_resource(show_spinner=False)
+def getwordcloud(path2font, width, height, words, mask, backgroundcolor="#1DA1F2"):
     wordclo = WordCloud(
         repeat=False,
         font_path=path2font,
@@ -112,5 +49,6 @@ def getwordcloud(path2font, width, height,
         background_color=backgroundcolor,
         stopwords=STOPWORDS,
         min_word_length=2,
-        mask=mask).generate(words)
+        mask=mask,
+    ).generate(words)
     return wordclo
